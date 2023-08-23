9589cae8f196be5b35b04867018df18dfe25e2b6
milka_deluxe <milagalko1@gmail.com>
2023-08-23 22:20:23 +0300

    add: full app

---
+++ expenses-app/src/components/Expenses/Expenses.js
@@ -0,0 +1,28 @@
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem.js";
+import Card from "../UI/Card.js";
+
+function Expenses(props) {
+    return (
+        <Card className="expenses">
+            <ExpenseItem
+                amount={props.items[0].amount}
+                title={props.items[0].title}
+                date={props.items[0].date} />
+            <ExpenseItem
+                amount={props.items[1].amount}
+                title={props.items[1].title}
+                date={props.items[1].date} />
+            <ExpenseItem
+                amount={props.items[2].amount}
+                title={props.items[2].title}
+                date={props.items[2].date} />
+            <ExpenseItem
+                amount={props.items[3].amount}
+                title={props.items[3].title}
+                date={props.items[3].date} />
+        </Card>
+    );
+}
+
+export default Expenses;
