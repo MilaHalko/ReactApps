d60b456d70f241239611a467ab1656a3c9562895
milka_deluxe <milagalko1@gmail.com>
2023-08-29 00:57:07 +0300

    add: expenses filter

--- expenses-app/src/components/Expenses/Expenses.js
+++ expenses-app/src/components/Expenses/Expenses.js
@@ -1,25 +1,26 @@
+import React, {useState} from 'react';
 import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem.js";
 import Card from "../UI/Card.js";
-import ExpensesFilter from "./ExpenseFilter/ExpensesFilter";
-import React, { useState } from 'react';
-
+import ExpensesFilter from "./ExpensesFilter/ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
-    const [filteredYear, setFilteredYear] = useState('2023');
 
-    const filterChahgeHandler = selectedYear => {
-        setFilteredYear(selectedYear);
-    }
+    const [filteredYear, setFilteredYear] = useState("2022")
+    const filterChangeHandler = selectedYear => setFilteredYear(selectedYear)
+
+    const filteredExpenses = props.items.filter(expense => expense.date.getFullYear().toString() === filteredYear)
 
     return (
         <div>
             <Card className="expenses">
-                <ExpensesFilter onChangeFilter={filterChahgeHandler} year={filteredYear} />
-                {props.items.map(expense => <ExpenseItem amount={expense.amount} title={expense.title} date={expense.date}/>)}
+                <ExpensesFilter onChangeFilter={filterChangeHandler} year={filteredYear}/>
+                <ExpensesChart expenses={filteredExpenses}/>
+                <ExpensesList items={filteredExpenses}/>
             </Card>
         </div>
-    );
+    )
 }
 
 export default Expenses;
