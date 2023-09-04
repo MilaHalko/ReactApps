099c04371a623e07532b8575c5a39e0e5f999a51
milka_deluxe <milagalko1@gmail.com>
2023-09-04 11:13:24 +0300

    add: course-goals-app css

--- course-goals-app/src/components/CourseGoals/CourseInput/CourseInput.js
+++ course-goals-app/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,24 +1,33 @@
-import './CourseInput.css'
 import React from "react";
 import Button from "../../UI/Button/Button";
+import styles from'./CourseInput.module.css'
 
 export default function CourseInput(props) {
-
     const [enteredValue, setEnteredValue] = React.useState('')
+    const [isValid, setIsValid] = React.useState(true)
+
     const submitHandler = (event) => {
         event.preventDefault()
+        if (enteredValue.trim().length === 0) {
+            setIsValid(false)
+            return
+        }
         props.onSubmitGoal(enteredValue)
         setEnteredValue('')
     }
     const inputChangeHandler = (event) => {
+        enteredValue.trim().length > 0 && setIsValid(true)
         setEnteredValue(event.target.value)
     }
 
     return (
         <form onSubmit={submitHandler}>
-            <div className='form-control'>
+            <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
                 <label>Course Goal</label>
-                <input onChange={inputChangeHandler} value={enteredValue}/>
+                <input
+                    onChange={inputChangeHandler}
+                    value={enteredValue}
+                />
             </div>
 
             <Button type='submit'>Add Goal</Button>
--- course-goals-app/src/components/UI/Button/Button.js
+++ course-goals-app/src/components/UI/Button/Button.js
@@ -1,9 +1,29 @@
-import './Button.css'
+import styled from "styled-components";
 
-export default function Button(props) {
-    return (
-        <button type={props.type} className='button' onClick={props.onClick}>
-            {props.children}
-        </button>
-    )
-}
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  background: #8b005d;
+  color: white;
+  border: 1px solid #8b005d;
+  cursor: pointer;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  width: 100%;
+  
+  @media (min-width: 600px) {
+    width: auto;
+  }
+  
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`
+
+export default Button
