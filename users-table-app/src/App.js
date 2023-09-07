bbdd8b5429c67a46c1aeb681103ff1b6e202083f
milka_deluxe <milagalko1@gmail.com>
2023-09-08 00:11:09 +0300

    add: app styles & basic structure with data processing

--- users-table-app/src/App.js
+++ users-table-app/src/App.js
@@ -1,10 +1,20 @@
+import UserInput from "./components/UserInput/UserInput";
+import UsersTable from "./components/UsersTable/UsersTable";
+import React from "react";
 
-function App() {
-  return (
-      <div>
-        <h1>Milka`s project</h1>
-      </div>
-  );
+export default function App() {
+    const [usersList, setUsersList] = React.useState([])
+
+    const addUserHandler = (userData) => {
+        setUsersList(prevList => {
+            return [...prevList, {name: userData.name, age: userData.age, id: Math.random().toString()}]
+        })
+    }
+
+    return (
+        <div>
+            <UserInput onAddUser={addUserHandler}/>
+            <UsersTable usersData={usersList}/>
+        </div>
+    );
 }
-
-export default App;
