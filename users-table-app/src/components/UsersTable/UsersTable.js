c1d52592dad4b19eb40b4f861882ec50c886a445
milka_deluxe <milagalko1@gmail.com>
2023-09-08 23:50:46 +0300

    add: "no users were added" message

--- users-table-app/src/components/UsersTable/UsersTable.js
+++ users-table-app/src/components/UsersTable/UsersTable.js
@@ -4,6 +4,9 @@ import Card from "../UI/Card";
 export default function UsersTable(props) {
     return (
         <Card className={classes.table}>
+            {props.usersData.length === 0 && <p className={classes["no-users"]}>
+                No users haven`t been added yet</p>
+            }
             <ul>
                 {props.usersData.map(userData => (
                     <li key={userData.id}>
